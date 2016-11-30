e3e1f94d17358c6b6d4acc9e91a840acd98cfbb6
mp3jjk <jhjung@lanada.kaist.ac.kr>
2016-11-30 18:30:51 +0900

    Modifying jkof

---
+++ lanada/param.h
@@ -0,0 +1,8 @@
+/* Adding simple remaining energy and alpha JJH*/
+#define INITIAL_ENERGY 200
+#define ALPHA 1
+extern uint8_t remaining_energy;
+extern uint8_t alpha;
+
+#define RPL_ENERGY_MODE 1
+#define LONG_ETX_PENALTY 5
